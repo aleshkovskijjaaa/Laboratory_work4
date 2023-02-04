cb2e3e39fb6ef9b87c11f4c806a5cd3e5ab88917
aleshkovskijjaaa <aleshkovskijjaa@edu.spbstu.ru>
2023-02-04 22:26:56 +0300

    Переделал сеттер и геттер

--- main.py
+++ main.py
@@ -1,20 +1,30 @@
 class Guitars:
-    def __init__(self, string: [int] = None, manufacture: [str] = None):
+    def __init__(self, string: int, manufacture: int):
         """
         Создан родительский класс Guitars
         :param string: количество струн
         :param manufacture: производитель
         """
-        self._string = None
-        self.manufacture = None
+        self.__string = 0
+        self.__manufacture = 0
 
-    @property
-    def stringw(self):
+    def get_string(self):
+        """
+        Создан интерфейсный метод (геттер) для возврата значения, установленного в атрибут self._sring
+        :return:
+        """
         return self._string
 
-    @stringw.setter
-    def stringw(self, new_string):
-        if (new_string < 1):
-            raise ValueError("Количество струн не может быть меньше одной")
-        print("Это сеттер метод")
-        self._string = new_string
+    def set_stringq(self, string, manufacture):
+        if type(string) in (int) and type(manufacture) in (str):
+            self.__string = string
+            self.__manufacture = manufacture
+        else:
+            raise ValueError("Передаваемые значения должны быть типов \"int\" и \"str\"")
+
+        """
+        Создан интерфейсный метод (сеттер) для установки значения атрибуту self._sring
+        :param new_string:
+        :return:
+        """
+
